6a2bcf9c367b9a825ab8cdab38a3ffe940aec01d
Ryan Block <rblock@gmail.com>
2024-07-24 22:03:24 -0700

    Oh, I guess I no longer live there

--- src/site/content.js
+++ src/site/content.js
@@ -1,7 +1,7 @@
 module.exports = /* html */
 `<div>
   <h1>👋🏻 Hi! I'm Ryan.</h1>
-  <h2>I live in San Francisco and I (help) build things on the internet! 🌈</h2>
+  <h2>I (help) build things on the internet! 🌈</h2>
   <p>Right now I'm working on a new company, called <a href="https://begin.com">Begin</a>.</p>
   <p>It's a tool to help people build the software of the future: serverlessly.</p>
   <p>I'm super biased, but it's pretty neat.</p>
